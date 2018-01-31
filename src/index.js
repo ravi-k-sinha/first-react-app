b8ac2012380eec97873eff0ce6b6dde84069d991
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-01-31 17:53:45 +0530

    Implemented time-travel so that game play can be rewinded to a previous run

--- src/index.js
+++ src/index.js
@@ -49,11 +49,19 @@ function Square(props) {
         history: [{
           squares: Array(9).fill(null),
         }],
+        stepNumber : 0,
         xIsNext: true,
       }
     }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       
@@ -63,14 +71,24 @@ function Square(props) {
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         history: history.concat([{squares: squares}]),
+        stepNumber: history.length,
         xIsNext : !this.state.xIsNext,
       });
     }
     render() {
 
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ? 'Go to move#' + move : 'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
 
       let status;
       if (winner) {
@@ -90,7 +108,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
